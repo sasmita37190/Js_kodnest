e56fa80707c3e7a7de793aa1ef64435ddac9f343
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2026-06-05 22:22:43 +0530

    changes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -57,3 +57,36 @@ console.log(typeof myFunction);
 // Object (native or host and does implement [[Call]])	=     "function"
 // Object (host and does not implement [[Call]])	
 // Implementation-defined except may not be "undefined", "boolean", "number", or "string".
+
+
+
+
+
+// ---------------------------:Memory----------------------------------------------
+
+
+//1.Stack (Primitive), Heap (Non-Primitive)
+
+
+let myyName ="sasmitaytbcom";
+anothername ="lizaytbchannel"
+
+console.log(myyName)
+console.log(anothername)
+
+
+let userOne = {
+    email: "subu@google.com",
+     upi: "rinku@ybl"
+}
+let userTwo = userOne;
+
+userTwo.email = "sulekha@google.com"
+
+console.log(userOne.email) 
+console.log(userTwo.email) 
+
+//Note: In JavaScript, primitive values are immutable, meaning they cannot be changed after they are created.
+//  When you assign a primitive value to a variable, it is stored directly in the variable. 
+// On the other hand, non-primitive values (objects) are mutable and are stored as references in the variable. When you assign an object to a variable,
+//  it stores a reference to the location of the object in memory. Therefore, when you modify the object through one variable, it affects all variables that reference the same object.
