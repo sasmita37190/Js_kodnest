8e57f5c76ac5efb85ba44c7a76330ae9e56f0c8c
Sasmita Sahoo <lizasasmitasahoo83@gmail.com>
2026-08-09 19:49:48 +0530

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,36 @@
+// const name = "sasmita";
+// const age = 23;
+
+//console.log(name + " is " + age + " years old.")
+//console.log(`Hello my name is ${name} and I am ${age} years old.`)
+
+
+// const gameName = new String('sasmita sahoo')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+
+// const newStringOne = "  hitesh "
+// console.log(newStringOne.trim());
+
+// const url = "https://www.youtube.com/watch?v=1gk5J6j3G7s&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=2"
+// console.log(url.replace("https://www.youtube.com/watch?v=1gk5J6j3G7s&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=2", "https://www.google.com/"));
+
+// console.log(url.includes("youtube"));
+
+// console.log(gameName.split(","))
+
+let name = "JavaScript";
+
+console.log(name.slice(4));
